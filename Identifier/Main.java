2d941f0e9c971eadaced67d907011b8e268c05a1
chakriyadavitgithub <chakriyadav163@gmail.com>
2024-08-28 17:22:34 +0530

    Basics

---
+++ Identifier/Main.java
@@ -0,0 +1,44 @@
+// public class Main {
+// //here we can use the string type also
+//     static int String=101; 
+//     public static void main(String[] args) {
+      
+//         System.out.println(String);
+//     }
+// }
+
+// public class Main{
+//     ///by using the float at the end we can use last (f)
+//     public static void main(String[] args) {
+//     float rating=4.7f;
+//         System.out.println(rating);
+//     }
+// }
+
+
+public class Main{
+    public static void main(String[] args){
+        ///Primitive data types;
+byte b=10;
+short s=100;
+int price=500000;
+long mobile=9494839573l;
+
+float esal1=4500.00f;
+double esal2=6000.00;
+double rating=4.5;
+
+char avail='y';
+boolean discount=true;
+
+System.out.println(b);
+System.out.println(s);
+System.out.println(price);
+System.out.println(mobile);
+System.out.println(esal1);
+System.out.println(esal2);
+System.out.println(rating);
+System.out.println(avail);
+System.out.println(discount);
+}
+}
