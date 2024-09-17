d362bdd9254c23ce5e9d333f3febf29652a28021
chakriyadavitgithub <chakriyadav163@gmail.com>
2024-09-17 12:26:24 +0530

    package

---
+++ AMS/Test.java
@@ -0,0 +1,7 @@
+final class A{//from using the final & the error will be occur
+    //bc0z we can't create the child class.
+
+}
+class B extends A{
+
+}
---
+++ PACKAGE/User.java
@@ -0,0 +1,12 @@
+package com.hdfc.user;
+class User{
+    public static void main(String[] args) {
+        System.out.println("GM");
+    }
+}
+
+// //from exceute process.
+// //java -d . User.java
+// //tree /F
+// //java com.hdec.user.User
+
