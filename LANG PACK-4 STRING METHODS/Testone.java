d24ec700c325212fca7ec26a3df531571b62862f
chakriyadavitgithub <chakriyadav163@gmail.com>
2024-10-04 17:03:06 +0530

    Stringmethod

---
+++ LANG PACK-4 STRING METHODS/Testone.java
@@ -0,0 +1,9 @@
+public class Testone {
+    public static void main(String[] args) {
+        String ename="Chakri";
+        System.out.println(ename.toUpperCase());
+    }
+    
+}
+///charAt()- retutn char of specified index
+///indexof()-return index of specified char
